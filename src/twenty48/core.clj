6e6de02e645027c395360b62cd033f9b9be40f27
Ketan Sangle <ketansa@thoughtworks.com>
2019-02-22 14:46:18 +0530

    (move right and left)

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,15 +1,21 @@
 (ns twenty48.core
   (:gen-class))
 
+(def partition (comp (partial mapcat (partial partition-all 2 2)) (partial partition-by identity) (partial remove zero?)))
+
+(def pad-zeros (comp (partial take 4) (partial flatten) (partial conj (repeat 4 0))))
+
+(def move (comp (partial pad-zeros) (partial map (partial apply +)) (partial partition)))
+
 (defn move-grid-right
   "Moves an entire grid to the right"
   [grid]
-  grid)
+  (map reverse (map move (map reverse grid))))
 
 (defn move-grid-left
   "Moves an entire grid to the left"
   [grid]
-  grid)
+  (map move grid))
 
 (defn move-grid-down
   "Moves an entire grid down"
